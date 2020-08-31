fb1bfa06a75bc2ac4c435ae591020df32ea7b8a3
Matilda Höglund <hoglund.matilda@gmail.com>
2020-08-31 15:56:16 +0200

    comments fixed in tests

--- tests/unit/tester.spec.js
+++ tests/unit/tester.spec.js
@@ -1,33 +1,31 @@
 //kommentarer om vad vi vill testa
 
-
 //CART
-1. Vill kolla att rendererar varukorgselementet
-2. Kolla att värdet i varukorgen matchar antalet produkter
-3. Kolla att sidan uppdateras när man ändrar produktantal
-4. Antal produkter skall uppdateras i localstorage / backend
-5. Testa att varukorgen blir tom när klickar på checkout
-6. Testa att summan av alla produkter stämmer
+//1. Vill kolla att rendererar varukorgselementet
+//2. Kolla att värdet i varukorgen matchar antalet produkter
+//3. Kolla att sidan uppdateras när man ändrar produktantal
+//4. Antal produkter skall uppdateras i localstorage / backend
+//5. Testa att varukorgen blir tom när klickar på checkout
+//6. Testa att summan av alla produkter stämmer
 
-//ShoeSelection
-1. Skall rendera ut antal produkter som finns i jsonfil eller backend
-2. Testa att när du klickar på en sko skall den öppna detaljerad sida för den skon
-
+//Products
+//1. Skall rendera ut antal produkter som finns i jsonfil eller backend
+//2. Testa att när du klickar på en sko skall den öppna detaljerad sida för den skon
 
 //Searchfield
 
-1. Sökfältet skall vara "" vid rendering
-2. Testa att input filtrerar skorna korrekt
-3. 
+//1. Sökfältet skall vara "" vid rendering
+//2. Testa att input filtrerar skorna korrekt
+//3.
 
 //ShoeDetailView
-1. Behöver testa att det går att lägga till produkt i varukorgen
-2. Behöver kolla att den lägger till vald storlek
+//1. Behöver testa att det går att lägga till produkt i varukorgen
+//2. Behöver kolla att den lägger till vald storlek
 
-1. i dropdowns för storlek och pris behöver vi kolla att det är vad man valt
+//1. i dropdowns för storlek och pris behöver vi kolla att det är vad man valt
 
-1. kolla att alla komponenter existerar och renderas
+//1. kolla att alla komponenter existerar och renderas
 
-2.
+//2.
 
-3. kolla så rätt route visas när man byter view
+//3. kolla så rätt route visas när man byter view
