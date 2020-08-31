6e24c0a8b0beb2e357228f846d42618028341567
Stefan Isgren <stefan.isgren@gmail.com>
2020-08-31 14:57:18 +0200

    .

---
+++ tests/unit/tester.spec.js
@@ -0,0 +1,25 @@
+//kommentarer om vad vi vill testa
+
+
+//CART
+1. Vill kolla att rendererar varukorgselementet
+2. Kolla att värdet i varukorgen matchar antalet produkter
+3. Kolla att sidan uppdateras när man ändrar produktantal
+4. Antal produkter skall uppdateras i localstorage / backend
+5. Testa att varukorgen blir tom när klickar på checkout
+6. Testa att summan av alla produkter stämmer
+
+//ShoeSelection
+1. Skall rendera ut antal produkter som finns i jsonfil eller backend
+2. Testa att när du klickar på en sko skall den öppna detaljerad sida för den skon
+
+
+//Searchfield
+
+1. Sökfältet skall vara "" vid rendering
+2. Testa att input filtrerar skorna korrekt
+3. 
+
+//ShoeDetailView
+1. Behöver testa att det går att lägga till produkt i varukorgen
+2. Behöver kolla att den lägger till vald storlek
