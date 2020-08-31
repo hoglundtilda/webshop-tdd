2e72810c9ebc5d08018d04d04c590ccf12c1ff1a
Matilda Höglund <hoglund.matilda@gmail.com>
2020-08-31 14:00:36 +0200

    variables, scss npm

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,11 @@
 </template>
 
 
+<style lang="scss" scoped>
+@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@100&display=swap");
+@import "./assets/scss/variables";
+
+#app {
+  font-family: $font;
+}
+</style>
