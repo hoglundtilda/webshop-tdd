8c1143367cf14fae840bbae5fbef3e743b76713e
Stefan Isgren <stefan.isgren@gmail.com>
2020-09-01 12:27:44 +0200

    wrote testing for footer
    written tests for footer component.

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="footer">
+    <a href="https://www.twitter.com">
+      <img src alt />
+    </a>
+    <a href="https://www.instagram.com">
+      <img src alt />
+    </a>
+    <a href="https://www.facebook.com">
+      <img src alt />
+    </a>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
