088ba36bbb7e8acd2724a8dc08384cc2ff7e76b7
Stefan Isgren <stefan.isgren@gmail.com>
2020-09-01 15:29:04 +0200

    updated newssection component
    Started building newssection component. fixed articles and background images.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
+    <NewsSection />
     <Footer />
   </div>
 </template>
 
 <script>
 import Footer from "@/components/Footer";
+import NewsSection from "@/components/NewsSection";
 export default {
   name: "Home",
   components: {
     Footer,
+    NewsSection,
   },
 };
 </script>
