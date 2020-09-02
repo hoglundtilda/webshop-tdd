d88218d47fe0a9b63f22ffc783d0fcffeb49a20f
Matilda Höglund <hoglund.matilda@gmail.com>
2020-09-02 09:30:02 +0200

    header test + component

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <Nav />
+    <section class="hero">
+      <div class="img_container">
+        <img src="../assets/img/header.png" alt="shoes" />
+      </div>
+      <div class="call_to_action">
+        <h1>Nya märken</h1>
+        <span>Höst 2020</span>
+        <button>Kollektion</button>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Nav from "./Nav";
+export default {
+  components: { Nav },
+};
+</script>
