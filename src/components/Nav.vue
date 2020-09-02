ecad609209adb1eece9b4581199984e6bdbc80af
Matilda Höglund <hoglund.matilda@gmail.com>
2020-09-02 11:17:18 +0200

    cartButton + test klar

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="nav">
-    <CartButton />
+    <ul>
+      <p>Om</p>
+      <p>Nyhter</p>
+      <p>Kollektion</p>
+      <p>Rea</p>
+    </ul>
+
+    <CartButton class="cart_button" />
   </div>
 </template>
 
@@ -10,3 +17,26 @@ export default {
   components: { CartButton },
 };
 </script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables";
+
+.nav {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  ul {
+    display: flex;
+
+    p {
+      margin: 1.5em;
+      color: $white;
+    }
+  }
+
+  .cart_button {
+    margin: 1em;
+  }
+}
+</style>
--- tests/unit/header.spec.js
+++ tests/unit/header.spec.js
@@ -1,18 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
 import Header from '@/components/Header.vue';
 import Nav from '@/components/Nav.vue';
-import CartButton from '@/components/CartButton.vue';
 
 it('should display Nav component when rendered', () => {
   const wrapper = shallowMount(Header),
     nav = wrapper.findComponent(Nav);
 
   expect(nav.exists()).toBe(true);
-});
-
-it('should display Cart component when rendered', () => {
-  const wrapper = shallowMount(Nav),
-    cart = wrapper.findComponent(CartButton);
-
-  expect(cart.exists()).toBe(true);
 });
