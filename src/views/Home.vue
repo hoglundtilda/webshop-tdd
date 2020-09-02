789fb4c713db4f1fd7d4938ba8bd75c7356addff
Matilda Höglund <hoglund.matilda@gmail.com>
2020-09-02 09:34:48 +0200

    header import i home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,24 @@
 <template>
   <div class="home">
-
+    <Header />
     <NewsSection />
     <Products />
     <Footer />
-
   </div>
 </template>
 
 <script>
-
+import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import NewsSection from "@/components/NewsSection";
 import Products from "@/components/Products.vue";
 export default {
   name: "Home",
   components: {
+    Header,
     Footer,
     NewsSection,
-     Products
+    Products,
   },
-
 };
 </script>
