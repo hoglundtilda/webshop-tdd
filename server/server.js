460c7110740e012e423bf239b6081fdcc8b817ee
Stefan Isgren <stefan.isgren@gmail.com>
2020-09-08 12:38:39 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,3 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/", serveStatic(path.join(__dirname, "../dist")));
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+console.log(`app is listening on port: ${port}`);
