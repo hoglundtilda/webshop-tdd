9b89e0ea569561f089d3238097fb49a49ab7f248
Stefan Isgren <stefan.isgren@gmail.com>
2020-09-08 12:30:05 +0200

    .

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path');
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+const express = require("express");
+const serveStatic = require("serve-static");
+const path = require("path");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use("/", serveStatic(path.join(__dirname, "../dist")));
