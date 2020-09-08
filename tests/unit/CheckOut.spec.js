d4c00f40c2dfd721fc61830257bc231d47318fc5
Stefan Isgren <stefan.isgren@gmail.com>
2020-09-08 08:14:20 +0200

    fixed test for to payment

--- tests/unit/CheckOut.spec.js
+++ tests/unit/CheckOut.spec.js
@@ -1,53 +1,244 @@
 import getters from "@/store/modules/getters.js";
 import actions from "@/store/modules/actions.js";
 import mutations from "@/store/modules/mutations.js";
+import CheckOut from "@/components/CheckOut";
+import Payment from "@/views/Payment";
+import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
+import routes from "@/router/routes.js";
+import VueRouter from "vue-router";
+import { sync } from "vuex-router-sync";
+import Vuex from "vuex";
+import store from "@/store/index.js";
+import App from "@/App";
 
-test("Returns the total price of all the products in the shoppingcart with free shipping", () => {
-  const state = {
-    shoppingCart: [
-      { qty: 2, price: 895 },
-      { qty: 1, price: 1395 },
-      { qty: 3, price: 4495 },
-    ],
-  };
-  const result = getters.totalPrice(state);
-  expect(result).toBe(16670);
-});
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+localVue.use(Vuex);
 
-test("Returns the total price of all the products in the shoppingcart with deliverycost", () => {
-  const state = {
-    shoppingCart: [
-      { qty: 2, price: 895 },
-      { qty: 1, price: 1395 },
-      { qty: 3, price: 4495 },
-    ],
-  };
-  const result = getters.totalPriceWithDeliveryCost(state);
-  expect(result).toBe(16739);
-});
+const router = new VueRouter({ routes });
 
-describe("", () => {
-  it("Should test the dispatch is being called with correct params", async () => {
+describe("CheckOut", () => {
+  let shoppingCart, product, getters;
+  beforeEach(() => {
+    getters = {
+      totalPrice: () => 3985,
+      totalPriceWithDeliveryCost: () => 4200,
+    };
+    product = {
+      id: "0004",
+      brand: "Iceberg",
+      name: "KAKKOI",
+      type: "Höga sneakers",
+      price: "4495,00",
+      color: "Multicolor",
+      material: {
+        Yttermaterial: "Konstmaterial/textil",
+        Foder: "Textil",
+        Innersula: "Textil",
+        Sula: "Konstmaterial",
+        Fodertjocklek: "Tunt foder",
+      },
+      productinfo: {
+        Tå: "Rund",
+        "Klack/sula": "Platt",
+        Förslutning: "Elastisk snörning",
+        Mönster: "Enfärgat",
+        Artikelnummer: "IC312N002-T11",
+      },
+      sizes: "38",
+      images: [
+        "iceberg_kakkoi/iceberg_kakkoi_1.png",
+        "iceberg_kakkoi/iceberg_kakkoi_2.png",
+        "iceberg_kakkoi/iceberg_kakkoi_3.png",
+      ],
+      qty: 1,
+      totalPrice: 4495,
+    };
+    shoppingCart = [
+      {
+        id: "0014",
+        brand: "DKNY",
+        name: "BREA",
+        type: "Slip-ins",
+        price: "889,00",
+        color: "Red",
+        material: {
+          Yttermaterial: "Konstmaterial/textil",
+          Foder: "Textil",
+          Innersula: "Textil",
+          Sula: "Konstmaterial",
+          Fodertjocklek: "Tunt foder",
+        },
+        productinfo: {
+          Tå: "Rund",
+          "Klack/sula": "Platt",
+          Förslutning: "Elastisk snörning",
+          Mönster: "Enfärgat",
+          Artikelnummer: "DK111A06H-G11",
+        },
+        sizes: "36",
+        images: [
+          "dkny_3/image_14.png",
+          "dkny_3/image_15.png",
+          "dkny_3/image_16.png",
+        ],
+        qty: 1,
+        totalPrice: 889,
+      },
+      {
+        id: "0019",
+        brand: "Jordan",
+        name: "WESTBROOK ONE TAKE",
+        type: "Indoorskor",
+        price: "1279,00",
+        color: "Multicolor",
+        material: {
+          Yttermaterial: "Konstmaterial/textil",
+          Foder: "Textil",
+          Innersula: "Textil",
+          Sula: "Konstmaterial",
+          Fodertjocklek: "Tunt foder",
+        },
+        productinfo: {
+          Tå: "Rund",
+          "Klack/sula": "Platt",
+          Förslutning: "Elastisk snörning",
+          Mönster: "Enfärgat",
+          Artikelnummer: "JOC42A02W-J11",
+        },
+        sizes: "40",
+        images: [
+          "jordans_4/image_29.png",
+          "jordans_4/image_30.png",
+          "jordans_4/image_31.png",
+        ],
+        qty: 1,
+        totalPrice: 1279,
+      },
+      {
+        id: "0022",
+        brand: "Steve Madden",
+        name: "CHAOS",
+        type: "Höga Sneakers",
+        price: "1145,00",
+        color: "Black/Multicolorr",
+        material: {
+          Yttermaterial: "Konstmaterial/textil",
+          Foder: "Textil",
+          Innersula: "Textil",
+          Sula: "Konstmaterial",
+          Fodertjocklek: "Tunt foder",
+        },
+        productinfo: {
+          Tå: "Rund",
+          "Klack/sula": "Platt",
+          Förslutning: "Elastisk snörning",
+          Mönster: "Enfärgat",
+          Artikelnummer: "ST312N00H-T11",
+        },
+        sizes: "39",
+        images: [
+          "steve_madden_2/image_38.png",
+          "steve_madden_2/image_39.png",
+          "steve_madden_2/image_40.png",
+        ],
+        qty: 1,
+        totalPrice: 1145,
+      },
+      {
+        id: "0020",
+        brand: "Nike Sportwear",
+        name: "AIR MAX 90 UNISEX",
+        type: "Sneakers",
+        price: "1499,00",
+        color: "Black/Green/Red",
+        material: {
+          Yttermaterial: "Konstmaterial/textil",
+          Foder: "Textil",
+          Innersula: "Textil",
+          Sula: "Konstmaterial",
+          Fodertjocklek: "Tunt foder",
+        },
+        productinfo: {
+          Tå: "Rund",
+          "Klack/sula": "Platt",
+          Förslutning: "Elastisk snörning",
+          Mönster: "Enfärgat",
+          Artikelnummer: "NI115O01I-Q11",
+        },
+        sizes: "39",
+        images: [
+          "nike_air_max_2/image_32.png",
+          "nike_air_max_2/image_33.png",
+          "nike_air_max_2/image_34.png",
+        ],
+        qty: 1,
+        totalPrice: 1499,
+      },
+    ];
+  });
+  test("Should return the price of all the products in the shoppingcart without deliverycost", () => {
+    const state = {
+      shoppingCart,
+    };
+    const expectedValue = 3985;
+    const result = getters.totalPrice(state);
+    expect(result).toBe(expectedValue);
+  });
+  test("Should return the price of all the products in the shoppingcart with deliverycost", () => {
+    const state = {
+      shoppingCart,
+    };
+    const expectedValue = 4200;
+    const result = getters.totalPriceWithDeliveryCost(state);
+    expect(result).toBe(expectedValue);
+  });
+  test("Should test emptyCart action to be dispatched with correct params", async () => {
     const commit = jest.fn();
     const ctx = { commit };
     await actions.emptyCart(ctx);
     expect(commit).toHaveBeenCalledWith("emptyCart");
   });
-});
-
-describe("Mutations", () => {
-  it("Should remove all the products from the shoppingCart", () => {
+  test("Should test emptyCart mutation to remove all the products from the shoppingCart", () => {
     const state = {
-      shoppingCart: [
-        { qty: 2, price: 895 },
-        { qty: 2, price: 895 },
-        { qty: 2, price: 895 },
-        { qty: 2, price: 895 },
-        { qty: 2, price: 895 },
-        { qty: 2, price: 895 },
-      ],
+      shoppingCart,
     };
     mutations.emptyCart(state);
     expect(state.shoppingCart).toStrictEqual([]);
+  });
+
+  it("renders a child component via routing", async () => {
+    const wrapper = mount(CheckOut, {
+      localVue,
+      store,
+      router,
+    });
+
+    const spy = jest.spyOn(router, "push");
+    const route = "/Payment";
+
+    await wrapper.find(".checkout_button").trigger("click");
+    await wrapper.vm.$nextTick();
+
+    expect(spy).toHaveBeenCalledWith(route);
+  });
+
+  test("Should test emptyCart dispatch when clicking the remove button", async () => {
+    const shoppingCart = [product];
+    const mockStore = {
+      state: {
+        shoppingCart,
+        product,
+      },
+      getters,
+      dispatch: jest.fn(),
+    };
+    const wrapper = mount(CheckOut, {
+      mocks: {
+        $store: mockStore,
+      },
+    });
+    await wrapper.find(".empty_cart").trigger("click");
+    expect(mockStore.dispatch).toHaveBeenCalledWith("emptyCart");
   });
 });
--- tests/unit/cartproduct.spec.js
+++ tests/unit/cartproduct.spec.js
@@ -1,82 +1,207 @@
 import mutations from "@/store/modules/mutations.js";
 import actions from "@/store/modules/actions.js";
+import { mount } from "@vue/test-utils";
 import CartProduct from "@/components/CartProduct";
-import { mount, shallowMount, createLocalVue } from "@vue/test-utils";
 
-it("Should test the dispatch is being called with correct params", async () => {
-  const commit = jest.fn();
-  const ctx = { commit };
-  const product = { qty: 1, price: 1395 };
-  await actions.removeFromCart(ctx, product);
-  expect(commit).toHaveBeenCalledWith("removeFromCart", product);
-});
-
-it("Should remove the selected product from the shoppingcart", () => {
-  const product = { qty: 1, price: 1395 };
-  const state = {
-    shoppingCart: [product, { qty: 2, price: 1895 }, { qty: 1, price: 4495 }],
-  };
-  mutations.removeFromCart(state, product);
-  expect(state.shoppingCart.indexOf(product)).toBe(-1);
-});
-
-it("Should update the quantity of the product based on value in the input field", () => {
-  const product = { qty: 1, price: "1395" };
-  const value = 5;
-
-  const expectedValue = value;
-
-  const object = { product: product, value: value };
-  const state = {};
-  mutations.updateQty(state, object);
-  expect(object.product.qty).toBe(expectedValue);
-});
-
-it("dispatches an action when a button is clicked", async () => {
-  const product = {
-    id: "0003",
-    brand: "Fila",
-    name: "ORBIT ZEPPA",
-    type: "Sneakers",
-    price: "879,00",
-    color: "Black",
-    material: {
-      Yttermaterial: "Skinn och textil",
-      Foder: "Textil",
-      Innersula: "Skinnimitation",
-      Sula: "Konstmaterial",
-      Fodertjocklek: "Tunt foder",
-    },
-    productinfo: {
-      Tå: "Rund",
-      "Klack/Sula": "Genomgående platå",
-      Förslutning: "Snörning",
-      Mönster: "Enfärgat",
-      Artikelnummer: "1FI11A005-Q11",
-    },
-    sizes: "40",
-    images: [
-      "fila/fila_orbit_zeppa_1.png",
-      "fila/fila_orbit_zeppa_2.png",
-      "fila/fila_orbit_zeppa_3.png",
-    ],
-    qty: 1,
-    totalPrice: 879,
-  };
-  const mockStore = {
-    dispatch: jest.fn(),
-    state: {
-      shoppingCart: [product],
-    },
-  };
-
-  const wrapper = mount(CartProduct, {
-    mocks: {
-      $store: mockStore,
-    },
+describe("CartProduct", () => {
+  let product, shoppingCart;
+  beforeEach(() => {
+    shoppingCart = [
+      {
+        id: "0014",
+        brand: "DKNY",
+        name: "BREA",
+        type: "Slip-ins",
+        price: "889,00",
+        color: "Red",
+        material: {
+          Yttermaterial: "Konstmaterial/textil",
+          Foder: "Textil",
+          Innersula: "Textil",
+          Sula: "Konstmaterial",
+          Fodertjocklek: "Tunt foder",
+        },
+        productinfo: {
+          Tå: "Rund",
+          "Klack/sula": "Platt",
+          Förslutning: "Elastisk snörning",
+          Mönster: "Enfärgat",
+          Artikelnummer: "DK111A06H-G11",
+        },
+        sizes: "36",
+        images: [
+          "dkny_3/image_14.png",
+          "dkny_3/image_15.png",
+          "dkny_3/image_16.png",
+        ],
+        qty: 1,
+        totalPrice: 889,
+      },
+      {
+        id: "0019",
+        brand: "Jordan",
+        name: "WESTBROOK ONE TAKE",
+        type: "Indoorskor",
+        price: "1279,00",
+        color: "Multicolor",
+        material: {
+          Yttermaterial: "Konstmaterial/textil",
+          Foder: "Textil",
+          Innersula: "Textil",
+          Sula: "Konstmaterial",
+          Fodertjocklek: "Tunt foder",
+        },
+        productinfo: {
+          Tå: "Rund",
+          "Klack/sula": "Platt",
+          Förslutning: "Elastisk snörning",
+          Mönster: "Enfärgat",
+          Artikelnummer: "JOC42A02W-J11",
+        },
+        sizes: "40",
+        images: [
+          "jordans_4/image_29.png",
+          "jordans_4/image_30.png",
+          "jordans_4/image_31.png",
+        ],
+        qty: 1,
+        totalPrice: 1279,
+      },
+      {
+        id: "0022",
+        brand: "Steve Madden",
+        name: "CHAOS",
+        type: "Höga Sneakers",
+        price: "1145,00",
+        color: "Black/Multicolorr",
+        material: {
+          Yttermaterial: "Konstmaterial/textil",
+          Foder: "Textil",
+          Innersula: "Textil",
+          Sula: "Konstmaterial",
+          Fodertjocklek: "Tunt foder",
+        },
+        productinfo: {
+          Tå: "Rund",
+          "Klack/sula": "Platt",
+          Förslutning: "Elastisk snörning",
+          Mönster: "Enfärgat",
+          Artikelnummer: "ST312N00H-T11",
+        },
+        sizes: "39",
+        images: [
+          "steve_madden_2/image_38.png",
+          "steve_madden_2/image_39.png",
+          "steve_madden_2/image_40.png",
+        ],
+        qty: 1,
+        totalPrice: 1145,
+      },
+      {
+        id: "0020",
+        brand: "Nike Sportwear",
+        name: "AIR MAX 90 UNISEX",
+        type: "Sneakers",
+        price: "1499,00",
+        color: "Black/Green/Red",
+        material: {
+          Yttermaterial: "Konstmaterial/textil",
+          Foder: "Textil",
+          Innersula: "Textil",
+          Sula: "Konstmaterial",
+          Fodertjocklek: "Tunt foder",
+        },
+        productinfo: {
+          Tå: "Rund",
+          "Klack/sula": "Platt",
+          Förslutning: "Elastisk snörning",
+          Mönster: "Enfärgat",
+          Artikelnummer: "NI115O01I-Q11",
+        },
+        sizes: "39",
+        images: [
+          "nike_air_max_2/image_32.png",
+          "nike_air_max_2/image_33.png",
+          "nike_air_max_2/image_34.png",
+        ],
+        qty: 1,
+        totalPrice: 1499,
+      },
+    ];
+    product = {
+      id: "0004",
+      brand: "Iceberg",
+      name: "KAKKOI",
+      type: "Höga sneakers",
+      price: "4495,00",
+      color: "Multicolor",
+      material: {
+        Yttermaterial: "Konstmaterial/textil",
+        Foder: "Textil",
+        Innersula: "Textil",
+        Sula: "Konstmaterial",
+        Fodertjocklek: "Tunt foder",
+      },
+      productinfo: {
+        Tå: "Rund",
+        "Klack/sula": "Platt",
+        Förslutning: "Elastisk snörning",
+        Mönster: "Enfärgat",
+        Artikelnummer: "IC312N002-T11",
+      },
+      sizes: "38",
+      images: [
+        "iceberg_kakkoi/iceberg_kakkoi_1.png",
+        "iceberg_kakkoi/iceberg_kakkoi_2.png",
+        "iceberg_kakkoi/iceberg_kakkoi_3.png",
+      ],
+      qty: 1,
+      totalPrice: 4495,
+    };
   });
-
-  await wrapper.find(".remove").trigger("click");
-
-  expect(mockStore.dispatch).toHaveBeenCalledWith("removeFromCart", product);
+  test("Should test removeFromCart dispatch when clicking the remove button", async () => {
+    const shoppingCart = [product];
+    const mockStore = {
+      state: {
+        shoppingCart,
+        product,
+      },
+      dispatch: jest.fn(),
+    };
+    const wrapper = mount(CartProduct, {
+      mocks: {
+        $store: mockStore,
+      },
+    });
+    await wrapper.find(".remove").trigger("click");
+    expect(mockStore.dispatch).toHaveBeenCalledWith("removeFromCart", product);
+  });
+  it("Should test removeFromCart action is being called with the correct params", async () => {
+    const commit = jest.fn();
+    const ctx = { commit };
+    await actions.removeFromCart(ctx, product);
+    expect(commit).toHaveBeenCalledWith("removeFromCart", product);
+  });
+  it("Should test removeFromCart mutation and remove the selected product from the shoppingcart", () => {
+    const product = { qty: 1, price: 1395 };
+    const state = {
+      shoppingCart,
+    };
+    mutations.removeFromCart(state, product);
+    expect(state.shoppingCart.indexOf(product)).toBe(-1);
+  });
+  it("Should test updateQty action is being called with the correct params", async () => {
+    const commit = jest.fn();
+    const ctx = { commit };
+    await actions.updateQty(ctx, product);
+    expect(commit).toHaveBeenCalledWith("updateQty", product);
+  });
+  it("Should test updateQty mutations so it sets the value of the input field as quantity of the product", () => {
+    const value = 5;
+    const expectedValue = value;
+    const object = { product: product, value: value };
+    const state = {};
+    mutations.updateQty(state, object);
+    expect(object.product.qty).toBe(expectedValue);
+  });
 });
